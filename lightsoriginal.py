9d626d53c5ad899bd5ba2f345faaaf043de4e4c1
Koji Ino <141215551+Person20020@users.noreply.github.com>
2025-01-18 08:23:02 -1000

    Update lightsoriginal.py

--- lightsoriginal.py
+++ lightsoriginal.py
@@ -10,7 +10,7 @@ GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
 def downstairs_bedroom(channel):
-        requests.get("https://api-v2.voicemonkey.io/trigger?token=1eaef0c009d4493c7f56ed423d8664f9_a864915ae61a09310edca8249aebf5c2&device=downstairs-bedroom")
+        requests.get("https://api-v2.voicemonkey.io/trigger?token=<api key here>&device=downstairs-bedroom")
 
 GPIO.add_event_detect(pin, GPIO.FALLING, callback=downstairs_bedroom, bouncetime=100)
 
